3f8bc0eaa4ff9b95cb33f28e12cada2853ea2840
Mateus Trindade <mateus_2607@hotmail.com>
2021-06-19 16:25:04 -0300

    Atualizar settings

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get(
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['infinite-castle-67836.herokuapp.com']
+ALLOWED_HOSTS = ['infinite-castle-67836.herokuapp.com','127.0.0.1']
 
 
 # Application definition
